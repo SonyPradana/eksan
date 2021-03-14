faaefe34d7ca0ccaac903abd7319c71f737d8c0e
Angger Pradana <sonypradana@gmail.com>
2021-03-14 16:08:39 +0700

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+<!-- html -->
+  <h1>Nama Saya</h1>
+  <h2>{{ nama }}</h2>
+</template>
+
+<script>
+// js
+export default {
+  data() {
+    return {
+      nama: 'eksan'
+    }
+  },
+}
+</script>
+
+<style scoped>
+/* css */
+h1 {
+  color: red;
+}
+</style>
